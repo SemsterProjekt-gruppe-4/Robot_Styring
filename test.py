c561fa44c108c67f51bffe6e5f93ffe4b97b82e1
Bosssebastian <54078225+Bosssebastian@users.noreply.github.com>
2025-11-12 15:05:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # read sensors
 from machine import Pin, ADC
+import time
 # greycode to step throgh all sensors
 sensor_seq=[
     [0,0,0],
@@ -11,16 +12,26 @@ sensor_seq=[
     [1,0,1],
     [1,0,0]
 ]
-pins = [Pin(12, Pin.OUT), Pin(13, Pin.OUT), Pin(14, Pin.OUT)]
-li = ADC(0)
+p1 = Pin(14, Pin.OUT)
+p2 = Pin(13, Pin.OUT)
+p3 = Pin(12, Pin.OUT)
+li = ADC(26)
 sensor_data = [0,0,0,0,0,0,0,0]
+
+
 
 while True:
     for i in range(8):
-        for pin in pins:
-            pins[pin] = sensor_seq[i][pin]
-        sensor_data[i] = (li.read_u16() * 3.3 /65536)
-    print(sensor_data)    
+        p1.value(sensor_seq[i][0])
+        p2.value(sensor_seq[i][1])
+        p3.value(sensor_seq[i][2])
+        time.sleep(0.05)
+        sensor_data[i] = li.read_u16()
+        
+    print(sensor_data)
+
+
+     
 
 '''
 # calculate how much to turn

2f9103f5be3a71a3e2065a14a8b41c6939cf63b9
Bosssebastian <54078225+Bosssebastian@users.noreply.github.com>
2025-11-24 09:38:28 +0100

    Update main.py
    New it works wirelessly

--- main.py
+++ main.py
@@ -1,51 +1,85 @@
-# Example usage
-# - Insert your main loop here, if any, and how you includes your StepperMotor class
-from machine import ADC, Timer
+#listener message
+from machine import Pin, ADC
 from stepper_motor import step_motor
-from diff_drive import diffdrive
+import network
+import socket
 import time
+import _thread
 
-li = ADC(0)
+INTERNET_Name = "Maskinens telefon"
+INTERNET_PASSWORD = "frederik"
 
-# fuction to collect data
-def save(t):
-    with open('log.csv', 'a') as log:
-        log.write(str(li.read_u16() * 3.3 /65536)+'\n')
+moter1 = step_motor(0,1,2,3)
+moter2 = step_motor(4,5,6,7)
 
+moter1.set_PWM(30)
+moter2.set_PWM(30)
 
-timer = Timer()
-timer.init(freq=100, mode=Timer.PERIODIC, callback=save)
+moter1.set_microsteps(12)
+moter2.set_microsteps(12)
 
-# Initialize the stepper motors with their respective GPIO pins
-motor1 = step_motor(0,1,2,3)
-motor2 = step_motor(4,5,6,7)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(INTERNET_Name, INTERNET_PASSWORD)
 
+max_wait = 10
+while max_wait>0:
+    if wlan.status()< 0 or wlan.status()>=3:
+        break
+    max_wait -= 1        
+    print('waiting for connection')
+    time.sleep(1)
 
-# Set PWM duty cycle for both motors
-motor1.set_PWM(20)
-motor2.set_PWM(20)
-
-
-# Initialize the differential drive system with the two motors, wheel diameter, and number of microsteps per step
-train = diffdrive(motor1, motor2, 267, 20)
-
-time.sleep(2)
-
-# for loop to turn right and left over the black line to
-for _ in range(1):
-    # Turn right 180 degrees
-    train.turn(180,"r")
-
-    # Release motors
-    motor1.release()
-    motor2.release()
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    status = wlan.ifconfig()
+    print('ip='+status[0])
     
-    # Turn left 180 degrees
-    train.turn(180,"l")
-    
-    # Release motors    
-    motor1.release()
-    motor2.release()
+addr = socket.getaddrinfo ('172.20.10.10', 80)[0][-1]
 
-# stops the collection of data
-timer.deinit()
+s=socket.socket()
+s.connect(addr)
+
+print('listening on', addr)
+direction = None
+def drive():
+    t = True
+    global direction
+    while t:
+        if direction == "FORWARDS":
+            moter1.set_direction(1)
+            moter2.set_direction(1)
+            moter1.step()
+            moter2.step()
+        elif direction == "BACKWARDS":
+            moter1.set_direction(-1)
+            moter2.set_direction(-1)
+            moter1.step()
+            moter2.step()
+        elif direction == "LEFT":
+            moter1.set_direction(1)
+            moter2.set_direction(-1)
+            moter1.step()
+            moter2.step()
+        elif direction == "RIGHT":
+            moter1.set_direction(-1)
+            moter2.set_direction(1)
+            moter1.step()
+            moter2.step()
+        elif direction == "stop":
+            t = False
+            break
+        else:
+            moter1.release()
+            moter2.release()
+
+
+_thread.start_new_thread(drive, ())
+while True:
+    try:
+        direction = s.recv(1024).decode()
+        print(direction)
+    except OSError as e:
+        direction = "stop"

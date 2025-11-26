354cfccde422d6cce81e027fce19aaf64db0ef53
Bosssebastian <54078225+Bosssebastian@users.noreply.github.com>
2025-11-26 13:36:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #listener message
-from machine import Pin, ADC
+from machine import Pin, ADC, PWM
 from stepper_motor import step_motor
 import network
 import socket
 import time
 import _thread
 
-INTERNET_Name = "Maskinens telefon"
-INTERNET_PASSWORD = "frederik"
+INTERNET_Name = "Bosssebastian's Phone"
+INTERNET_PASSWORD = "123456789"
+
+led = PWM(8, freq=1000)
+delay = 0.001
 
 moter1 = step_motor(0,1,2,3)
 moter2 = step_motor(4,5,6,7)
@@ -37,7 +40,7 @@ else:
     status = wlan.ifconfig()
     print('ip='+status[0])
     
-addr = socket.getaddrinfo ('172.20.10.10', 80)[0][-1]
+addr = socket.getaddrinfo ('10.179.176.63', 80)[0][-1]
 
 s=socket.socket()
 s.connect(addr)
@@ -46,40 +49,61 @@ print('listening on', addr)
 direction = None
 def drive():
     t = True
-    global direction
+    global direction, delay
     while t:
-        if direction == "FORWARDS":
-            moter1.set_direction(1)
-            moter2.set_direction(1)
-            moter1.step()
-            moter2.step()
-        elif direction == "BACKWARDS":
-            moter1.set_direction(-1)
-            moter2.set_direction(-1)
-            moter1.step()
-            moter2.step()
-        elif direction == "LEFT":
-            moter1.set_direction(1)
-            moter2.set_direction(-1)
-            moter1.step()
-            moter2.step()
-        elif direction == "RIGHT":
-            moter1.set_direction(-1)
-            moter2.set_direction(1)
-            moter1.step()
-            moter2.step()
-        elif direction == "stop":
-            t = False
-            break
-        else:
+        try:
+            time.sleep(delay)
+            if direction == "FORWARDS":
+                moter1.set_direction(1)
+                moter2.set_direction(1)
+                moter1.step()
+                moter2.step()
+                moter1.release()
+                moter2.release()
+            elif direction == "BACKWARDS":
+                moter1.set_direction(-1)
+                moter2.set_direction(-1)
+                moter1.step()
+                moter2.step()
+                moter1.release()
+                moter2.release()
+            elif direction == "LEFT":
+                moter1.set_direction(1)
+                moter2.set_direction(-1)
+                moter1.step()
+                moter2.step()
+                moter1.release()
+                moter2.release()
+            elif direction == "RIGHT":
+                moter1.set_direction(-1)
+                moter2.set_direction(1)
+                moter1.step()
+                moter2.step()
+                moter1.release()
+                moter2.release()
+            elif direction == "stop":
+                moter1.release()
+                moter2.release()
+                t = False
+                break
+            else:
+                moter1.release()
+                moter2.release()
+        except:
             moter1.release()
             moter2.release()
+        
 
 
 _thread.start_new_thread(drive, ())
 while True:
     try:
-        direction = s.recv(1024).decode()
-        print(direction)
+        try:
+            direction, delay = s.recv(1024).decode().split(',')
+            delay = 0.005 - (65535 - int(delay)) / 65535 * 0.000999 * 5
+            led.duty_u16(int(65535 * (1-10*delay)))
+        except ValueError:
+            pass
     except OSError as e:
         direction = "stop"
+
